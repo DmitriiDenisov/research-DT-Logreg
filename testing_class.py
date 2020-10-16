5ffc507330e891ab756a18b7e9e622d6eb017df8
DmitriiDenisov <dmitryhse@gmail.com>
2020-10-16 15:26:05 +0400

    change limits for plots

--- testing_class.py
+++ testing_class.py
@@ -6,22 +6,73 @@ import numpy as np
 from RTTree import RTTree, NumpyEncoder
 from utils import plot_2D
 
+"""
+Example 0
 np.random.seed(7)
-
 X, y = make_circles(n_samples=200, noise=0.3, factor=0.2, )
 transformation = [[0.6, -0.6], [-0.1, 0.6]]
 X = np.dot(X, transformation)
 y[y == 0] = -1
+"""
+
+"""
+# Example 1a
+np.random.seed(17)
+n1 = 50
+n2 = 50
+center_x1 = np.array([-3, 0])
+center_x2 = np.array([3, 0])
+
+X1 = center_x1 + np.random.randn(n1, 2)
+X2 = center_x2 + np.random.randn(n2, 2)
+"""
+
+"""
+# Example 1b
+np.random.seed(17)
+n1 = 50
+n2 = 50
+center_x1 = np.array([-2, 0])
+center_x2 = np.array([2, 0])
+
+X1 = center_x1 + np.random.randn(n1, 2)
+X2 = center_x2 + np.random.randn(n2, 2)
+"""
+
+"""
+# Example 1c
+np.random.seed(17)
+n1 = 50
+n2 = 50
+center_x1 = np.array([-1.5, 0])
+center_x2 = np.array([1.5, 0])
+
+X1 = center_x1 + np.random.randn(n1, 2)
+X2 = center_x2 + np.random.randn(n2, 2)
+"""
+# Example 1d
+np.random.seed(17)
+n1 = 50
+n2 = 50
+center_x1 = np.array([-1.3, 0])
+center_x2 = np.array([1.3, 0])
+
+X1 = center_x1 + np.random.randn(n1, 2)
+X2 = center_x2 + np.random.randn(n2, 2)
+
+X = np.concatenate((X1, X2), axis=0)
+y = np.array([1] * n1 + [-1] * n2)
 
 temp = RTTree()
 temp.fit(X, y)
-pred = temp.predict(np.array([[1, 2], [0, 0], [0.5, -0.2], [1, -1.5]]))
+pred = temp.predict(np.array([[1, 2], [0, 0], [-1, -2], [-1, 2]]))
 
 print(json.dumps(temp.tree_struct, sort_keys=False, indent=4, cls=NumpyEncoder))
 # print(temp.tree_struct)
 print(pred)
 
-plot_2D(X, temp.tree_struct['Wopt'], y, temp)
+# X: np.array (n, 2), y: np.array (n,) (y consists from -1 and 1)
+plot_2D(X, temp.tree_struct['Wopt'], y, temp, title='Example 1d')
 
 # Example with (N, M)
 """
--- utils.py
+++ utils.py
@@ -10,13 +10,15 @@ def plot_rec(X, y, dict_struct):
     m1 = dict_struct['Wopt'][1]
     x_vals = np.linspace(X.min(), X.max(), 100)
     y_vals = -(m0 / m1) * x_vals - b / m1
-    plt.plot(x_vals, y_vals, color='blue')
+    plt.plot(x_vals, y_vals, label=dict_struct['Wopt'])
     plot_rec(X, y, dict_struct['left'])
     plot_rec(X, y, dict_struct['right'])
+
+    # plt.fill_between(x_vals, y1, y2, where=y2 > y1, facecolor='yellow', alpha=0.5)
     return True
 
 
-def plot_2D(X: np.array, W: np.array, y: np.array, tree):
+def plot_2D(X: np.array, W: np.array, y: np.array, tree, title: str):
     b = W[2]
     m0 = W[0]
     m1 = W[1]
@@ -24,14 +26,18 @@ def plot_2D(X: np.array, W: np.array, y: np.array, tree):
     x_vals = np.linspace(X.min(), X.max(), 100)
     y_vals = -(m0 / m1) * x_vals - b / m1
 
-    plt.scatter(X[y == -1][:, 0], X[y == -1][:, 1])
-    plt.scatter(X[y == 1][:, 0], X[y == 1][:, 1])
-    plt.plot(x_vals, y_vals, color='blue')
+    plt.scatter(X[y == -1][:, 0], X[y == -1][:, 1], label='y=-1')
+    plt.scatter(X[y == 1][:, 0], X[y == 1][:, 1], label='y=1')
+    plt.plot(x_vals, y_vals, label=tree.tree_struct['Wopt'])
 
     # recursive plot
     plot_rec(X, y, tree.tree_struct['left'])
     plot_rec(X, y, tree.tree_struct['right'])
 
-    plt.title('Separating line with custom cost')
+    plt.title(title)
     plt.grid(True)
+    y_max = np.max(X[:, 1])
+    y_min = np.min(X[:, 1])
+    plt.ylim(2 * y_min, 2 * y_max)
+    plt.legend()
     plt.show()
